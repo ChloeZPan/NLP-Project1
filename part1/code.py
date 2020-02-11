dac1d14adb6b8434175f1a8b0a2d2341b976b60b
Tianchu Xie <txie@unitedsolutions.local>
2020-02-11 15:57:58 -0500

    fixed

--- part1/code.py
+++ part1/code.py
@@ -10,14 +10,21 @@ def check_subsumption(type1, type2):
     """1) Take two types in the TRIPS ontology as inputs. If one type is a subtype of the other, return the tuple
             (True, type), where type is the type that is a subtype. Otherwise, return the tuple (False, None)."""
     # IMPLEMENT FUNCTION HERE
+    # def parent_lookup(goal, p):
+    #     if 'parent' in jsontrips.ontology()[p].keys():
+    #         if goal in jsontrips.ontology()[p]['parent']:
+    #             return True
+    #         else:
+    #             parent_lookup(goal, jsontrips.ontology()[p]['parent'])
+    #     else:
+    #         return False
     def parent_lookup(goal, p):
-        if 'parent' in jsontrips.ontology()[p].keys():
-            if goal in jsontrips.ontology()[p]['parent']:
+        current = p
+        while 'parent' in jsontrips.ontology()[current].keys() and jsontrips.ontology()[current]['parent'] != "":
+            if goal in jsontrips.ontology()[current]['parent']:
                 return True
-            else:
-                parent_lookup(goal, jsontrips.ontology()[p]['parent'])
-        else:
-            return False
+            current = jsontrips.ontology()[current]['parent']
+        return False
 
     if 'parent' in jsontrips.ontology()[type1].keys() and parent_lookup(type1, type2):
         return True, type2
